2e165ff6fcf4723de16d79b6a97550a2b65a580a
Sankar Naredla <60485876+sankarn0863@users.noreply.github.com>
2021-08-24 13:42:41 -0400

    Update Input.jsx

--- react/src/elements/Input.jsx
+++ react/src/elements/Input.jsx
@@ -33,7 +33,52 @@ export default class Input extends React.Component {
             return 0;
           }
 
-    console.log(calculateRewards(priceVal));      
+    console.log(calculateRewards(priceVal));  
+    
+//3 Months reward
+    class Transaction {
+      constructor(price) {
+          this.price = price;
+          this.rewards = calculateRewards(price);
+          this.transactionDate = new Date();
+      }
+  }
+  
+  class TransactionList {
+      constructor() {
+          this.list = [];
+      }
+  
+      getLast3MonthsList() {
+          var today = new Date();
+          const threeOldDate = today.setMonth(today.getMonth() - 3);
+          let filteredList = this.list.filter(trans => trans.transactionDate > threeOldDate);
+          return filteredList.sort((a,b) => b.transactionDate - a.transactionDate);
+      }
+  
+      getAllTransactions() {
+          return this.list.sort((a,b) => b.transactionDate-a.transactionDate);
+      }
+  
+      addTransaction(price) {
+          const transaction = new Transaction(price);
+          this.list.push(transaction);
+      }
+  
+      getTotalRewards() {
+          return this.list.length ? this.list.reduce((acc,key)=>key.rewards+acc, 0) : 0;
+      }
+  
+      rewardPerMonth() {
+          let last3MonthRewardsInDesc = [];
+          for(let i=0; i<3; i++) {
+              let filteredList = this.list.filter(trans => trans.transactionDate.getMonth() == (new Date).getMonth() - i );
+              last3MonthRewardsInDesc[i] = filteredList.reduce((acc,key)=>key.rewards+acc,0);
+          }
+          return last3MonthRewardsInDesc;
+      }
+  }
+    
 
   // REST API POST METHOD
 
@@ -104,6 +149,12 @@ export default class Input extends React.Component {
   }
 
 }
+
+
+
+
+
+
 
 
 
